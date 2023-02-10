57a38532c7094c20b50ce64d153daf453f7cd696
Adli I. Ifkar <adly.shadowbane@gmail.com>
2023-02-10 23:24:27 +0700

    Added Default error handler

--- pkg/server/httpresponse.go
+++ pkg/server/httpresponse.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -10,25 +11,28 @@ type SimpleHttpResponse struct {
 	Message string `json:"message"`
 }
 
-type DataMessage struct {
+type ErrorMessage struct {
+	Success bool      `json:"success"`
+	Data    ErrorData `json:"data"`
+}
+
+type ErrorData struct {
 	Message string `json:"message"`
 }
 
-func SendHttpResp(w http.ResponseWriter, messageString string, statusCode int) {
-	var requestStatus = true
-	if statusCode != 200 {
-		requestStatus = false
-	}
+func SendAndLogError(w http.ResponseWriter, m string, err error, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	zap.S().Errorf("%s %d: %v", m, statusCode, err.Error())
 
-	jsonResponse := SimpleHttpResponse{
-		Status:  requestStatus,
-		Message: messageString,
+	jsonResponse := ErrorMessage{
+		Success: false,
+		Data: ErrorData{
+			Message: err.Error(),
+		},
 	}
 
 	response, _ := json.Marshal(jsonResponse)
 
 	w.WriteHeader(statusCode)
-	w.Write(response)
-
-	return
+	_, _ = w.Write(response)
 }
