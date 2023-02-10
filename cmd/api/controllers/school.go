0153ba49e84001babe5d7a2f792f1441907e3068
Adli I. Ifkar <adly.shadowbane@gmail.com>
2023-02-10 23:25:05 +0700

    Fixed controller

--- cmd/api/controllers/school.go
+++ cmd/api/controllers/school.go
@@ -6,12 +6,10 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/shadowbane/golang-microservice-sekolah/cmd/models"
 	"github.com/shadowbane/golang-microservice-sekolah/pkg/application"
-	"go.uber.org/zap"
+	"github.com/shadowbane/golang-microservice-sekolah/pkg/server"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"runtime"
 	"strings"
 	"time"
 )
@@ -39,6 +37,7 @@ type schoolData struct {
 	NPSN          string    `json:"npsn"`
 	Bentuk        string    `json:"bentuk"`
 	Status        string    `json:"status"`
+	AlamatJalan   string    `json:"alamat_jalan"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
@@ -64,17 +63,12 @@ func SchoolIndex(app *application.Application) httprouter.Handle {
 		formattedValues.Success = true
 		formattedValues.Data = schools
 
-		//fmt.Printf("%#v\n", schools)
-
 		response, _ := json.Marshal(formattedValues)
 		_, err := w.Write(response)
 		if err != nil {
-			zap.S().Errorf(err.Error())
-			writeErrorResponse(w, http.StatusInternalServerError, "Internal Server Error")
-		}
+			server.SendAndLogError(w, "Internal Server Error", err, http.StatusInternalServerError)
 
-		if app.Cfg.GetAppEnv() != "production" {
-			PrintMemUsage()
+			return
 		}
 	}
 }
@@ -93,12 +87,7 @@ func SchoolShow(app *application.Application) httprouter.Handle {
 		var school schoolData
 
 		if err := app.DB.Table("schools").Where("uuid = ?", p.ByName("id")).First(&school).Error; err != nil {
-			zap.S().Errorf("Error %d: %s", http.StatusNotFound, err.Error())
-			http.Error(w, err.Error(), http.StatusNotFound)
-
-			if app.Cfg.GetAppEnv() != "production" {
-				PrintMemUsage()
-			}
+			server.SendAndLogError(w, "Not Found", err, http.StatusNotFound)
 
 			return
 		}
@@ -112,12 +101,9 @@ func SchoolShow(app *application.Application) httprouter.Handle {
 		response, _ := json.Marshal(formattedValues)
 		_, err := w.Write(response)
 		if err != nil {
-			zap.S().Errorf(err.Error())
-			writeErrorResponse(w, http.StatusInternalServerError, "Internal Server Error")
-		}
+			server.SendAndLogError(w, "Internal Server Error", err, http.StatusInternalServerError)
 
-		if app.Cfg.GetAppEnv() != "production" {
-			PrintMemUsage()
+			return
 		}
 	}
 }
@@ -129,7 +115,8 @@ func SchoolCreate(app *application.Application) httprouter.Handle {
 
 		err := json.NewDecoder(r.Body).Decode(&request)
 		if err != nil {
-			writeErrorResponse(w, http.StatusUnprocessableEntity, "Unprocessible Entity")
+			server.SendAndLogError(w, "Unprocessible Entity", err, http.StatusUnprocessableEntity)
+
 			return
 		}
 
@@ -144,8 +131,9 @@ func SchoolCreate(app *application.Application) httprouter.Handle {
 		response, _ := json.Marshal(formattedValues)
 		_, err = w.Write(response)
 		if err != nil {
-			zap.S().Errorf(err.Error())
-			writeErrorResponse(w, http.StatusInternalServerError, "Internal Server Error")
+			server.SendAndLogError(w, "Internal Server Error", err, http.StatusInternalServerError)
+
+			return
 		}
 	}
 }
@@ -158,11 +146,13 @@ func SchoolUpdate(app *application.Application) httprouter.Handle {
 		var request schoolData
 		err := json.NewDecoder(r.Body).Decode(&request)
 		if err != nil {
-			writeErrorResponse(w, http.StatusUnprocessableEntity, "Unprocessible Entity")
+			server.SendAndLogError(w, "Unprocessible Entity", err, http.StatusUnprocessableEntity)
+
 			return
 		}
 
-		app.DB.Table("schools").Where("uuid = ?", p.ByName("id")).Update(&request)
+		request.UUID = p.ByName("id")
+		app.DB.Table("schools").Where("uuid = ?", request.UUID).Update(&request)
 
 		var formattedValues returnData
 		formattedValues.Success = true
@@ -171,8 +161,9 @@ func SchoolUpdate(app *application.Application) httprouter.Handle {
 		response, _ := json.Marshal(formattedValues)
 		_, err = w.Write(response)
 		if err != nil {
-			zap.S().Errorf(err.Error())
-			writeErrorResponse(w, http.StatusInternalServerError, "Internal Server Error")
+			server.SendAndLogError(w, "Internal Server Error", err, http.StatusInternalServerError)
+
+			return
 		}
 	}
 }
@@ -180,6 +171,13 @@ func SchoolUpdate(app *application.Application) httprouter.Handle {
 func SchoolDelete(app *application.Application) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		w.Header().Set("Content-Type", "application/json")
+
+		var school schoolData
+		if err := app.DB.Table("schools").Where("uuid = ?", p.ByName("id")).First(&school).Error; err != nil {
+			server.SendAndLogError(w, "Not Found", err, http.StatusNotFound)
+
+			return
+		}
 
 		app.DB.Where("uuid = ?", p.ByName("id")).Delete(models.School{})
 
@@ -193,56 +191,9 @@ func SchoolDelete(app *application.Application) httprouter.Handle {
 		response, _ := json.Marshal(formattedValues)
 		_, err := w.Write(response)
 		if err != nil {
-			zap.S().Errorf(err.Error())
-			writeErrorResponse(w, http.StatusInternalServerError, "Internal Server Error")
+			server.SendAndLogError(w, "Internal Server Error", err, http.StatusInternalServerError)
+
+			return
 		}
 	}
-}
-
-// Writes the error response as a Standard API JSON response with a response code
-func writeErrorResponse(w http.ResponseWriter, errorCode int, errorMsg string) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(errorCode)
-	err := json.
-		NewEncoder(w).Encode(&JsonErrorResponse{Error: &ApiError{Status: errorCode, Title: errorMsg}})
-
-	if err != nil {
-		zap.S().Fatalf(err.Error())
-	}
-}
-
-//Populates a model from the params in the Handler
-func populateModelFromHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params, model interface{}) error {
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-	if err != nil {
-		return err
-	}
-	if err := r.Body.Close(); err != nil {
-		return err
-	}
-	if err := json.Unmarshal(body, model); err != nil {
-		return err
-	}
-	return nil
-}
-
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-
-	alloc := bToMb(m.Alloc)
-	total := bToMb(m.TotalAlloc)
-	sys := bToMb(m.Sys)
-
-	zap.S().Debugf("Alloc = %vMB, TotalAlloc = %vMB, Sys = %vMB", alloc, total, sys)
-
-	//// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	//fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	//fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	//fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	//fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
 }
